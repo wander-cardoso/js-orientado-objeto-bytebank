0be839100565af04c79494a5d1678d63bac4c666
Wander-Cardoso <wander.hsmc@gmail.com>
2024-06-10 22:17:28 +0100

    criação do metodo de sacar dentro da classe contaCorrente

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ class Cliente {
 class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+
+    }
 }
+
+
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
@@ -14,7 +23,7 @@ const cliente2 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
-cliente1.saldo = 0;
+cliente1.saldo = 100;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
@@ -22,9 +31,17 @@ cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
+
+contaCorrenteRicardo.saldo = 50
 contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(500);
 
+console.log(contaCorrenteRicardo.saldo);
 
-console.log(cliente1, contaCorrenteRicardo.saldo);
-console.log(cliente2);
+/*  
+console.log(cliente1,);
+console.log(cliente2);*/
+
